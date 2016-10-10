12c6c9d54df3de738139eb6fbeb26ca4ad4371cd
guoyaqi <1664216915@qq.com>
2016-10-10 19:54:01 +0800

    fix the error of user page

--- www/js/controllers/user.js
+++ www/js/controllers/user.js
@@ -1,19 +1,19 @@
 angular.module('starter.controllers')
 
 	.controller('UserCtrl', function($scope, $stateParams, $http, $location, $ionicPopup, Services) {
-			
-			
+
+
 //			var _userId = $location.search()['userid'];
-			var _baseUrl = "http://172.16.2.7:8080";
-			
+			var _baseUrl = "http://172.16.0.178:8080";
+
 			var _userId = "57c96c19d9f2822078df18b9";
-			
+
 			var resourcesType = $location.search()['type'];
-			
+
 			$scope.getResources = function(){
-				
+
 			}
-			
+
 			if(resourcesType!=undefined){
 				if(resourcesType==1){
 					$scope.title = "支持的创意"
@@ -28,21 +28,21 @@ angular.module('starter.controllers')
 					$scope.title = "收藏的资源"
 				}
 			}
-			
+
 			//获取用户信息
-			$scope.getUserInfo = function(){				
-				$http.get(_baseUrl+"/api/user?userId="+_userId).success(function(result){			
-						$scope.userinfo=result.data;	
-				});	
+			$scope.getUserInfo = function(){
+				$http.get(_baseUrl+"/api/user?userId="+_userId).success(function(result){
+						$scope.userinfo=result.data;
+				});
 			}
 			$scope.getUserInfo();
 
 //			$http({
 //				method:"POST",
 //				url:_baseUrl+"/api/user/likeProject?projectId="+item.id+"&add=true"
-//			}).success(function(result){			
+//			}).success(function(result){
 //				$scope.getProjectDetail();
-//			})					
-	
+//			})
+
 
 	})
