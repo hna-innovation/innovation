6bf5ea4a93d1d422b028066f2786ac9639b2e391
chenchong@qq.com <251226925@qq.com>
2016-10-10 17:31:08 +0800

    update lookuser

--- www/js/controllers/lookuser.js
+++ www/js/controllers/lookuser.js
@@ -1,6 +1,7 @@
 angular.module('starter.controllers')
 
 	.controller('lookuserCtrl', function($scope, $stateParams, $http, $location, $ionicPopup, Services) {
-		console.log("xxxx")
-
+		
+	
+			
 	})
