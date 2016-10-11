f978d68976a41963776321f6054ea9677cb4c0e9
ZHANGHD715 <hdzhang@thoughtworks.com>
2016-10-11 17:03:11 +0800

    move product footer directive to a single file.

---
+++ www/js/directives/hnaFooterNavDirective.js
@@ -0,0 +1,36 @@
+angular.module('starter.services')
+
+.directive("hnaFooternav", function () {
+    return {
+        restrict: "EAC",
+        replace: true,
+        transclude: false,
+        template :"<div class='detail-footer'>"+
+        				  "<div class='detail-footer-share'>"+
+        				  	"<a href='http://v.t.sina.com.cn/share/share.php?title={{detail.name}}&url={{shareUrl}}&pic={{imageUrls}}'><img src='../img/sina.png'></a>"+
+        				  	"<a href='http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url={{shareUrl}}&title={{detail.name}}&summary={{detail.description}}&pics={{imageUrls}}'><img src='../img/qzone.png'></a>"+
+        				  "</div>"+
+	        			  "<ul class='detail-footer-nav'>"+
+	        				"<li>{{detail.likeCount}}<i class='if' ng-class=\"{true:'i-aixinhover',false:'i-collect'}[detail.like]\" ng-click='like(detail)'></i></li>"+
+	        				"<li>{{detail.favoriteCount}}<i class='if' ng-class=\"{true:'i-star1',false:'i-star'}[detail.favorite]\" ng-click='favorite(detail)'></i></li>"+
+	        				"<li><i class='if i-xiaoxi'></i></li>"+
+	        				"<li><i class='if i-fenxiang' id='share'></i></li>"+
+	        			  "</ul>"+
+        			  "</div>",
+        	link : function(element){
+        		jQuery(document).on("click","#share",function(){
+        			var topNumber = jQuery(".detail-footer-share").css("top");
+        			if(topNumber=="0px"){
+        				jQuery(".detail-footer-share").animate({
+        					top:"-60px"
+        				})
+        			}
+        			if(topNumber=="-60px"){
+        				jQuery(".detail-footer-share").animate({
+        					top:"0px"
+        				})
+        			}
+        		})
+        	}
+	}
+});
