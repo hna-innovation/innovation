024e72b897234bff7d22fee34f1b0941a2df7d40
chenchong@qq.com <251226925@qq.com>
2016-10-11 09:36:41 +0800

    update detail

--- www/js/controllers/lookuser.js
+++ www/js/controllers/lookuser.js
@@ -1,7 +1,15 @@
 angular.module('starter.controllers')
 
 	.controller('lookuserCtrl', function($scope, $stateParams, $http, $location, $ionicPopup, Services) {
+
+		var _userId = "57c96c19d9f2822078df18b9";
 		
-	
+		$http({
+          method:"GET",
+          url: Services.getUrl('/api/draft?userId=' + _userId + '&page=0&size=8&sort=createdDate,desc')			
+		}).success(function(){
+			
+		})
+		
 			
 	})
