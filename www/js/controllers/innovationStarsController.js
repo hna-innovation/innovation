e81dab6bcb0a81708d2a4b75c5fa992ea5105d8b
ZHANGHD715 <hdzhang@thoughtworks.com>
2016-10-11 13:37:15 +0800

    feat: improved style of innovation stars page and added fake data.

--- www/js/controllers/innovationStarsController.js
+++ www/js/controllers/innovationStarsController.js
@@ -1,4 +1,38 @@
 angular.module('starter.controllers')
-  .controller('InnovationStarsCtrl', [function($scope, $state, Content) {
+  .controller('InnovationStarsCtrl', function($scope, $state, Content) {
     'use strict';
-  }]);
+
+    $scope.stars = [
+    	{
+    		name: "马涛",
+    		avator: "../img/user/user.jpg",
+    		ownedProductsCount: 5,
+    		likeTotalCount: 3115
+    	},
+    	{
+    		name: "马涛",
+    		avator: "../img/user/user.jpg",
+    		ownedProductsCount: 5,
+    		likeTotalCount: 3115
+    	},
+    	{
+    		name: "马涛",
+    		avator: "../img/user/user.jpg",
+    		ownedProductsCount: 5,
+    		likeTotalCount: 3115
+    	},
+    	{
+    		name: "马涛",
+    		avator: "../img/user/user.jpg",
+    		ownedProductsCount: 5,
+    		likeTotalCount: 3115
+    	},
+    	{
+    		name: "马涛",
+    		avator: "../img/user/user.jpg",
+    		ownedProductsCount: 5,
+    		likeTotalCount: 3115
+    	}
+    ];
+
+  });
