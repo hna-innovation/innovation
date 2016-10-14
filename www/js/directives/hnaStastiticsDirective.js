b6f1472232877567e39a21ae57437aafa5fd8b34
ZHANGHD715 <hdzhang@thoughtworks.com>
2016-10-14 22:48:31 +0800

    feat: replace the stastitics show with a directive.

---
+++ www/js/directives/hnaStastiticsDirective.js
@@ -0,0 +1,7 @@
+angular.module('starter')
+  .directive("hnaStastitics", function() {
+    return {
+      restrict: 'E',
+      templateUrl: '../../templates/components/hna-stastitics.html'
+    };
+  });
