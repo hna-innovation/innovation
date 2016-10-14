3431db257b05f3ea03daa950e72c5f12711e6355
Sichu <zhangsichu@gmail.com>
2016-10-14 16:35:11 +0800

    feat: set the api request url.

--- www/js/controllers/lastInnovationController.js
+++ www/js/controllers/lastInnovationController.js
@@ -1,4 +1,12 @@
 angular.module('starter.controllers')
-  .controller('LastInnovationCtrl', [function($scope, $state, Content) {
+  .controller('LastInnovationCtrl', function($scope, $state, ProjectsService) {
     'use strict';
-  }]);
+
+    $scope.getProjects = function(){
+      ProjectsService.getProductsByType('likeCount', 0).success(function(result){
+          $scope.projects=result.data;
+      });
+    }
+
+    $scope.getProjects();
+  });
