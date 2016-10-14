a72c57b59f71dc47cb55f85970848ace601cd75b
ZHANGHD715 <hdzhang@thoughtworks.com>
2016-10-14 23:51:24 +0800

    feat: added animation for stastitics show.

--- www/js/directives/hnaStastiticsDirective.js
+++ www/js/directives/hnaStastiticsDirective.js
@@ -2,6 +2,37 @@ angular.module('starter')
   .directive("hnaStastitics", function() {
     return {
       restrict: 'E',
-      templateUrl: '../../templates/components/hna-stastitics.html'
+      templateUrl: '../../templates/components/hna-stastitics.html',
+      controller: function($scope, $interval) {
+        var stopCount,
+            stopCountResources,
+            stopCountSuccesses;
+
+        $scope.innovationsCount = 0;
+        $scope.resourcesCount = 0;
+        $scope.successesCount = 0;
+
+        stopCountInnovations = $interval(function() {
+          $scope.innovationsCount++;
+
+          if ($scope.innovationsCount > 245)
+            $interval.cancel(stopCountInnovations);
+        }, 8);
+
+        stopCountResources = $interval(function() {
+          $scope.resourcesCount++;
+
+          if ($scope.resourcesCount > 36)
+            $interval.cancel(stopCountResources);
+        }, 8);
+
+        stopCountSuccesses = $interval(function() {
+          $scope.successesCount++;
+
+          if ($scope.successesCount > 25)
+            $interval.cancel(stopCountSuccesses);
+        }, 8);
+
+      }
     };
   });
