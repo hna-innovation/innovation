2d7eafaf496e6e9f112d7beb54a2b5325cc587d6
guoyaqi <1664216915@qq.com>
2016-10-14 16:36:59 +0800

    modify the api

--- www/js/constants/apiConstant.js
+++ www/js/constants/apiConstant.js
@@ -1,5 +1,5 @@
 angular.module('configuration')
     .constant('Api', {
         'SEARCH_API': '/api/show/search',
-        'PROJECTS_API': '/api/projects/custom'
+        'PROJECTS_API': '/api/projects'
     });
