4f13c55f9ef874f9ede10efadb5bb6d70b12ab86
ZHANGHD715 <hdzhang@thoughtworks.com>
2016-10-17 17:20:55 +0800

    added error message to user drafts page.

--- www/js/controllers/user/userDraftsController.js
+++ www/js/controllers/user/userDraftsController.js
@@ -1,23 +1,26 @@
 angular.module('starter.controllers')
   .controller('UserDraftsCtrl', UserDraftsCtrl);
 
-function UserDraftsCtrl($scope, UserService, Content, $state) {
+function UserDraftsCtrl($scope, UserService, Content, $state, HnaAlert) {
 
   $scope.NO_DRAFTS = Content.draft.NO_DRAFTS;
 
   UserService.getUserDrafts(function(result) {
-
-    if (result.data.content && result.data.content.length) {
-      $scope.drafts = result.data.content;
+    if (result.code === 0) {
+      if (result.data.content && result.data.content.length) {
+        $scope.drafts = result.data.content;
+      }
+    } else {
+      HnaAlert.default('获取用户创意草稿信息出错！');
     }
 
   }, function() {
-    // TODO
+    HnaAlert.default('获取用户创意草稿信息出错！');
   });
 
   $scope.goStep3 = function(draftId) {
     localStorage.draftId = draftId;
     $state.go('step-3');
-  }
+  };
 
 }
