4d9c89e0f09f552ef398a582cae0a2528d5082d0
Sichu <zhangsichu@gmail.com>
2016-10-19 16:02:53 +0800

    feat: update the user page language text.

--- www/js/constants/languageConstant.js
+++ www/js/constants/languageConstant.js
@@ -1,8 +1,8 @@
 angular.module('configuration')
   .constant('LANGUAGE', {
     'languages': {
-      'zh': 'cn_ZH',
-      'en': 'en_US'
+      'zh': '中文',
+      'en': '英文'
     },
     'preferredLanguage': 'zh'
   });
