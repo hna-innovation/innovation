c80c1181e2d4c3db32069770fd5d09fa2ad0d0b5
Sichu <zhangsichu@gmail.com>
2016-10-21 11:07:34 +0800

    feat: build css with the hash code.

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var sh = require('shelljs');
 var del = require('del');
 var path = require('path');
 var runSequence = require('run-sequence');
+var rev = require('gulp-rev');
+var inject = require('gulp-inject');
 
 var buildDir = path.resolve('build');
 
@@ -34,6 +36,7 @@ gulp.task('style-css', function (done) {
         .pipe(minifyCss({
           keepSpecialComments: 0
          }))
+        .pipe(rev())
         .pipe(gulp.dest(path.join(buildDir, 'css')))
         .on('end', done);
 })
@@ -44,6 +47,7 @@ gulp.task('style-resources', function (done) {
         .pipe(minifyCss({
           keepSpecialComments: 0
          }))
+        .pipe(rev())
         .pipe(gulp.dest(path.join(buildDir, 'css/resources')))
         .on('end', done);
 })
@@ -54,6 +58,7 @@ gulp.task('style-vendor', function (done) {
       .pipe(minifyCss({
         keepSpecialComments: 0
        }))
+      .pipe(rev())
       .pipe(gulp.dest(path.join(buildDir, 'css/vendor')))
       .on('end', done);
 })
@@ -64,6 +69,42 @@ gulp.task('image-copy', function (done) {
       .on('end', done);
 })
 
+gulp.task('js-copy', function (done) {
+  gulp.src('www/js/**/*.*')
+      .pipe(gulp.dest(path.join(buildDir, 'js')))
+      .on('end', done);
+})
+
+gulp.task('templates-copy', function (done) {
+  gulp.src('www/templates/**/*.*')
+      .pipe(gulp.dest(path.join(buildDir, 'templates')))
+      .on('end', done);
+})
+
+gulp.task('favicon-copy', function (done) {
+  gulp.src('www/favicon.ico')
+      .pipe(gulp.dest(buildDir))
+      .on('end', done);
+})
+
+gulp.task('inject-index', function (done) {
+
+  var _inject = function(src, tag) {
+    return inject(src, {
+      starttag: '<!-- inject:' + tag + ':{{ext}} -->',
+      addRootSlash: false
+    });
+  };
+
+  gulp.src('www/index.html')
+    .pipe(_inject(gulp.src("css/css*", { cwd: buildDir, read: false }), 'style-css'))
+    .pipe(_inject(gulp.src("css/resources/resources*", { cwd: buildDir, read: false }), 'style-resources'))
+    .pipe(_inject(gulp.src("css/vendor/vendor*", { cwd: buildDir, read: false }), 'style-vendor'))
+    .pipe(gulp.dest(buildDir))
+    .on('end', done);
+})
+
+
 gulp.task('build', function(done) {
   runSequence(
   'clean',
@@ -72,8 +113,9 @@ gulp.task('build', function(done) {
     'style-copy-icon', 'style-copy-lib', 'style-css', 'style-resources', 'style-vendor',
 
     // image files
-    'image-copy'
-  ])
+    'image-copy', 'js-copy', 'templates-copy', 'favicon-copy'
+  ],
+  'inject-index')
 });
 
 var paths = {
