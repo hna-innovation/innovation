4da7572eb2a4be97c2b12cc6767b5956b5ffc5d2
guoyaqi <1664216915@qq.com>
2016-10-21 21:36:15 +0800

    add link to resource list

--- www/js/controllers/resources/techResourceDetail.js
+++ www/js/controllers/resources/techResourceDetail.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers')
-  .controller('TechResourceDetailCtrl', function($scope, $state, Content) {
+  .controller('TechResourceDetailCtrl', function($scope, $state, $stateParams, Content, RESOURCE) {
     'use strict';
 
-
+    $scope.detail = RESOURCE.TECH.RESOURCES[$stateParams.detailId];
 
   });
