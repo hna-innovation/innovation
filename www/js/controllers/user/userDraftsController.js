7f8f4146fff752d45b05927a239767237dd730e9
Sichu <zhangsichu@gmail.com>
2016-10-25 11:55:09 +0800

    [#201] fix: fix the image size issue.

--- www/js/controllers/user/userDraftsController.js
+++ www/js/controllers/user/userDraftsController.js
@@ -6,14 +6,14 @@ function UserDraftsCtrl($scope, UserService, Content, $state, HnaAlert) {
   $scope.NO_DRAFTS = Content.draft.NO_DRAFTS;
 
   UserService.getUserDrafts(function (result) {
-    if (result.data.content && result.data.content.length) {
+    if (result.data && result.data.content && result.data.content.length) {
       $scope.drafts = result.data.content;
     } else {
       $scope.attentionMsg = Content.EMPTY_CONTENT;
     }
   }, function (error) {
     //出错信息统一在app.js处理,包括错误码401、404、504
-    // HnaAlert.default('获取用户创意草稿信息出错！');
+    //HnaAlert.default('获取用户创意草稿信息出错！');
     $scope.attentionMsg = Content.TIME_OUT;
   });
 
