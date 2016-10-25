62fa3927dbd01bca305f0ed10128cdb0356da7c4
Junli <jlli@thoughtworks.com>
2016-10-25 16:38:22 +0800

    [#221] Remove nav direction for innvation page after login

--- www/js/controllers/login/loginController.js
+++ www/js/controllers/login/loginController.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers')
-  .controller('LoginCtrl', function ($scope, $state, ModalServices, LoginService, HnaAlert) {
+  .controller('LoginCtrl', function ($scope, $state, ModalServices, LoginService, HnaAlert, $ionicViewSwitcher) {
     'use strict';
     $scope.goToRegister = function () {
       ModalServices.goToRegister();
@@ -32,6 +32,7 @@ angular.module('starter.controllers')
           localStorage.userHeaderIcon = result.data.headerIcon;
           $scope.closePopup();
           HnaAlert.defaultSuccess('登录成功!');
+          $ionicViewSwitcher.nextDirection('none');
           $state.reload($state.current);
         } else if (result.code == 1) {
           HnaAlert.defaultError('登录信息不正确!');
