6c64e91c9dd8e6a9347a3235de1dd720cbc62340
Junli <jlli@thoughtworks.com>
2016-10-31 17:43:18 +0800

    [#293] Replace swiper slides with slidingTabs

--- www/js/controllers/user/otherUserController.js
+++ www/js/controllers/user/otherUserController.js
@@ -12,37 +12,4 @@ angular.module('starter.controllers')
       }
     }, function () {
     });
-
-
-    $timeout(function () {
-      Swiper('#swiper', {
-        direction: 'horizontal',
-        loop: true,
-        autoplay: 0,
-        pagination: '.tab-nav',
-        paginationClickable: true,
-        paginationBulletRender: function (index) {
-          var tag = [{
-            name: "发布的创意"
-          }, {
-            name: "发布的资源"
-          }]
-          return "	<li class='swiper-pagination-bullet'><span>" + tag[index].name + "</span></li>"
-        },
-        onSlideChangeEnd: function (swiper) {
-          var obj = angular.element(document.getElementById("swiper-nav")).find("li");
-          var index = swiper.activeIndex;
-          if (index == (obj.length + 1)) {
-            index = 1;
-          } else if (index == 0) {
-            index = obj.length;
-          }
-          index--;
-          obj.removeClass("active");
-          obj.eq(index).addClass("active");
-        }
-      })
-    }, 1000);
-
-
-  })
+  });
