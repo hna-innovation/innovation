88a2e3bcdb3ead15fb1c88439847244e25af7989
ZHANGHD715 <hdzhang@thoughtworks.com>
2016-11-01 18:16:47 +0800

    [feat#302] added page for user to edit avator.

--- www/js/controllers/user/userEditController.js
+++ www/js/controllers/user/userEditController.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-	.controller('UserEditCtrl', function($scope, $stateParams, HnaAlert, $ionicHistory, $location, $ionicPopup, $state, UserService, Content, $ionicViewSwitcher) {
+	.controller('UserEditCtrl', function($scope, $stateParams, HnaAlert, $ionicHistory, $location, $ionicPopup, $state, UserService, Content, $ionicViewSwitcher, $timeout, ImageUploadService) {
       $scope.info = {target: $stateParams.target, text: $stateParams.text, length: $stateParams.length, mulitline: $stateParams.mulitline, required: $stateParams.required};
 
 			$scope.getUserInfo = function() {
@@ -12,12 +12,30 @@ angular.module('starter.controllers')
 	      });
 	    }
 
+			// 头像从本地选择完成后
+		  $scope.beforeChange = function(file) {
+		    $scope.loading = true;
+		  };
+
+		  // 头像上传
+		  $scope.uploadFiles = uploadImage;
+
       $scope.cancel = function() {
         $ionicViewSwitcher.nextDirection('back');
         $state.go('user-edit');
       }
 
       $scope.save = function() {
+				if($scope.info.target === 'headerIcon') {
+					saveAvator();
+				} else {
+					saveProfile();
+				}
+      }
+
+			$scope.getUserInfo();
+
+			function saveProfile() {
 				if($scope.info.required) {
 					var targetValue = $scope.userInfo[$scope.info.target]
 					if (targetValue == null || targetValue.length == 0) {
@@ -45,7 +63,65 @@ angular.module('starter.controllers')
 				}).error(function(error){
 					HnaAlert.default(Content.user.UPDATE_ERROR);
 				})
-      }
+			}
 
-			$scope.getUserInfo();
+			function saveAvator() {
+				// Todo
+			}
+
+			function uploadImage(file, errFiles) {
+		    var errFile = errFiles && errFiles[0];
+
+		    if (errFile) {
+		      _validateImgFile(errFile.$error);
+		      return;
+		    }
+
+		    if (file) {
+		      ImageUploadService
+		        .upload(file)
+		        .then(function(result) {
+		          $scope.loading = false;
+		          var response = result.data;
+
+		          $timeout(function() {
+		            if (response.code == 0) {
+		              _imageUploadSuccess(response);
+		              HnaAlert.default('图片上传成功！');
+		            } else {
+		              HnaAlert.default('图片上传失败！');
+		            }
+		          });
+
+		        },function() {
+		          $scope.loading = false;
+		          HnaAlert.default('图片上传失败！');
+		        });
+		    } else {
+		      $scope.loading = false;
+		    }
+		  }
+
+			function _imageUploadSuccess(response) {
+		    if (response.data) {
+		      $scope.userAvatorId = response.data[0].id;
+		      $scope.picURL = response.data[0].url;
+		      jQuery('#img-view').attr('src', $scope.picURL)
+		    }
+		  }
+
+			function _validateImgFile(errType) {
+		    // 限制上传类型和文件大小
+		    if ( errType === 'pattern') {
+		      HnaAlert.default('请选择正确的图片类型！');
+		      jQuery('#photoUpload').val('');
+		      return;
+		    }
+
+		    if ( errType === 'maxSize') {
+		      HnaAlert.default('图片大小不能超过3M！');
+		      jQuery('#photoUpload').val('');
+		      return;
+		    }
+		  }
 	})
--- www/js/services/imageUploadService.js
+++ www/js/services/imageUploadService.js
@@ -1,12 +1,12 @@
 angular.module('starter.services')
-  .factory('ImageUploadService', function(Upload, Api) {
+  .factory('ImageUploadService', function(Upload, Api, HelperService) {
     'use strict';
 
     var imageUploadService = {};
 
     imageUploadService.upload = function(file) {
       file.upload = Upload.upload({
-        url: Api.IMAGE_UPLOAD_API,
+        url: HelperService.getUrl(Api.IMAGE_UPLOAD_API),
         data: { file: file }
       });
 
