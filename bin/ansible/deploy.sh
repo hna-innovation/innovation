2c6da61eb50985e04e02b47b9146d8cf91b3cda7
Zhenwei Liu <zwliu@thoughtworks.com>
2016-11-07 10:42:25 +0800

    change innovation

--- bin/ansible/deploy.sh
+++ bin/ansible/deploy.sh
@@ -5,4 +5,4 @@ docker run --rm -v /var/lib/jenkins/workspace/innovation_mobile/:/var/lib/jenkin
 -e ANSIBLE_HOST_KEY_CHECKING=False \
 --workdir /var/lib/jenkins/workspace/innovation_mobile/bin/ansible \
 dockerhub.hnair.cloud/baselibrary/ansible:1 ansible-playbook -i environments/$1/inventory \
--e "VERSION=$2" -e "ENV=$1" innvoation-mobile.yml
+-e "VERSION=$2" -e "ENV=$1" innovation-mobile.yml
