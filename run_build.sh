86a383400971f7ca8eb2246156ea9dd943f4fc51
Zhenwei Liu <zwliu@thoughtworks.com>
2016-11-08 15:05:08 +0800

    test pact test

--- run_build.sh
+++ run_build.sh
@@ -7,7 +7,7 @@ NEXUS_USER="admin:admin123"
 
 docker run --rm -v /var/lib/jenkins/workspace/innovation_mobile:/var/lib/jenkins/workspace/innovation_mobile -v /root/.npm/:/root/.npm/ -v /root/.npmrc/:/root/.npmrc/ -v /root/.bowerrc/:/root/.bowerrc/ --workdir /var/lib/jenkins/workspace/innovation_mobile node:7.0-gulp \
 npm run pact && \
-&& curl -v -X PUT -H 'Content-Type: application/json' -d@test/pacts/mobile-site-innovation-api.json http://172.16.2.87:8000/pacts/provider/innovation-api/consumer/mobile-site/version/${1} &&\
+curl -v -X PUT -H 'Content-Type: application/json' -d@test/pacts/mobile-site-innovation-api.json http://172.16.2.87:8000/pacts/provider/innovation-api/consumer/mobile-site/version/${1} &&\
 gulp build
 
 tar zcf innovation-mobile-${VERSION}.$1.tar.gz build/
