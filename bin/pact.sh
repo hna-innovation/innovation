0ecd4e4459c396cb275a4aa791abdbbc2f1cbf48
冯永超 <fengyongchao@qq.com>
2017-02-09 14:43:57 +0800

    update pact url address

--- bin/pact.sh
+++ bin/pact.sh
@@ -1,3 +1,3 @@
 npm run pact
-curl -v -X PUT -H "Content-Type: application/json" -d@test/pacts/mobile-site-innovation-api.json http://172.16.2.87:8000/pacts/provider/innovation-api/consumer/mobile-site/version/2
+curl -v -X PUT -H "Content-Type: application/json" -d@test/pacts/mobile-site-innovation-api.json http://54.223.120.163:8000/pacts/provider/innovation-api/consumer/mobile-site/version/2
 
